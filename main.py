4dbf2803ff90f727af727d6f7eb255e640eada04
Sweetberla <issabella878@gmail.com>
2025-08-25 14:51:37 +0000

    venv created and pymongo installed

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
+#from oop import Chat
 
-import add
-import show
-import update
-import delete
-
-add_task_response = add.add_task("sleep")
-print(add_task_response)
-
-
-show_tasks_response = show.show_task()
-print(show_tasks_response)
-
-
-update_task_response = update.update_task("sleep", "Wake up")
-print(update_task_response)
-
-delete_task_response = delete.delete_task("Wake up")
-print(delete_task_response)
-
-
-
-
-
-
+#chat_with_abena = Chat()
+#print(chat_with_abena)
